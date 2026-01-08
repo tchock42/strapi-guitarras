21a1b78aaab866ff4fdaec6791ba5095dc4db22b
Jacob Gomez Carrillo <husky-coldjacob@hotmail.com>
2026-01-08 10:39:21 -0600

    added cert ssl

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
@@ -55,17 +56,17 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
+        ssl: env.bool('DATABASE_SSL', false) ? {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
+          ca: fs.readFileSync(path.join(__dirname, '../certs/prod-ca-2021.crt')).toString(),
           capath: env('DATABASE_SSL_CAPATH', undefined),
           cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool(
             'DATABASE_SSL_REJECT_UNAUTHORIZED',
             true
           ),
-        },
+        } : false,
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
